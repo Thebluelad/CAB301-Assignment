691e947d0c562252bc5ff5671658072d12b1cf04
thematthewest <matt@thebluelad.local>
2016-05-03 15:29:21 +1000

    Created and started the main c++ file and compiled to test it worked

---
+++ 301Assignment.cc
@@ -0,0 +1,29 @@
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+//Returns the median value in a given array A of n numbers. This is the kth element, where k = |n/2|, if the array was sorted.
+
+void bruteForceMedian(int a[], int n)
+{
+    int k = abs(n/2);
+    int numSmaller;
+    int numEqual;
+
+    for (int i = 0; i < n-1; i++)
+    {
+	
+    }
+}
+
+
+
+
+
+int main()
+{
+    return 0;
+}
