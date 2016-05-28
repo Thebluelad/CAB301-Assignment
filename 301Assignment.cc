50ff8db684361f4da73df07c79099a082c8a08e5
thematthewest <matt@thebluelad.local>
2016-05-28 20:04:22 +1000

    Fixed a whole shitload and added in some nice features like text file logging and csv output. Still working on some tests

--- 301Assignment.cc
+++ 301Assignment.cc
@@ -7,14 +7,38 @@
 
 using namespace std;
 
+//Constants
+const int STARTN = 0;
+const int FINISHN = 10000;
+const int INCREMENT = 50;
+const int NUMTESTS = 100;
+
+//Global Variables
+int comparisonsp = 0;
+int comparisonsb = 0;
+long totalb = 0;
+long totalp = 0;
+double totalTimeb = 0;
+double totalTimep = 0;
+
+//Function Declarations
+int bruteForceMedian(int a[], int n);
+int partitionMedian(int a[], int n);
 int select(int a[], int l, int m, int h);
-int comparisons = 0;
-double total = 0;
+int partition(int a[], int l, int h);
+int select(int a[], int l, int m, int h);
+void generateArray(int *a, int n, int seed);
+void functionalTests();
+void runComparisonsAndTimeData();
+
+//Output Files
+ofstream testLog;
+ofstream graphLog;
 
 //Returns the median value in a given array A of n numbers. This is the kth element, where k = |n/2|, if the array was sorted.
 int bruteForceMedian(int a[], int n)
 {
-    int k = ceil(n/2);
+    int k = ceil(n/2.0);
 
     for (int i = 0; i <= n-1; i++)
     {
@@ -22,7 +46,7 @@ int bruteForceMedian(int a[], int n)
 	int numEqual = 0;
 	for (int j = 0; j <= n-1; j++)
 	{
-	    comparisons++;
+	    comparisonsb++;
 	    if (a[j] < a[i])
 	    {
 		numSmaller++;
@@ -41,7 +65,7 @@ int bruteForceMedian(int a[], int n)
 	}
 
     }
-    return 42;
+    //return 42;
 }
 
 int partitionMedian(int a[], int n)
@@ -65,7 +89,7 @@ int partition(int a[], int l, int h)
 	if (a[j] < pivotval)
 	{
 	    //This is where the comparisons should happen
-	    comparisons++;
+	    comparisonsp++;
 	    pivotloc++;
 	    //Swaps elements around the pivot
 	    int temp = a[pivotloc];
@@ -97,75 +121,126 @@ int select(int a[], int l, int m, int h)
     {
 	return select(a, pos + 1, m, h);
     }
-    return 0;
+    //return 0;
 }
 
-int * generateArray(int n, int seed)
-{
-    srand(time(NULL) * seed^clock());
-    int a[n];
-    
-    for(int i = 0; i < n; i++)
-    {
-	int randGenerated = rand() % 10;
-	a[i] = randGenerated;
-	cout << randGenerated << " ";
-	cout << a[i] << endl;
-    }
-    cout << endl;
-    
-    return a;
-}
-
-void generateArray2(int *a, int n, int seed)
+void generateArray(int *b, int *a, int n, int seed)
 {
     srand(time(NULL) * seed^clock());
     for(int i = 0; i < n; i++)
     {
-	int randGenerated = rand();
+	int randGenerated = rand() % 10000;
 	a[i] = randGenerated;
-//	cout << randGenerated << " ";
-//	cout << a[i] << endl;
+	b[i] = randGenerated;
+//	cout << randGenerated << ",";
     }
-//    cout << endl;
+  //  cout << endl << endl;
 }
 
 
 int main()
 {
-    int n = 10000;
-    int numTests = 50;
-    int median;
-    int *p;
-    for (int j = 0; j <= n; j += 500)
-    {
-	for (int i = 0; i < 5; i++)
-	{
-	    for(int f = 0; f < numTests; f++)
-	    {
-		int generated[j];
-		p = generated;
-		generateArray2(p, j, f * j);
-		clock_t begin = clock();
-		median = partitionMedian(p, j);
-		clock_t end = clock();
-	    
-		double elapsed_secs = double(end - begin);
-//	    cout << j << "," << elapsed_secs << endl;
-	    
-
-//	    for (int i = 0; i < n; i++)
-//	    {
-//		cout << *(p + i) << " ";
-//	    }
-//	    cout << endl;
-		//total += comparisons;
-		total += elapsed_secs;
-		comparisons = 0;
-	    }
-	    cout << j << "," << total/numTests << endl;
-	    total = 0;
-	}
-    }
+    functionalTests();
+    //runComparisonsAndTimeData();
     return 0;
 }
+
+void runComparisonsAndTimeData()
+{
+    graphLog.open("graphLog.csv");
+
+    for (int j = STARTN; j <= FINISHN; j += INCREMENT)
+    {
+	int medianPartition;
+	int medianBruteForce;
+	for (int i = 0; i < 1; i++)
+	{
+	    for(int f = 0; f < NUMTESTS; f++)
+	    {
+		int *p;
+		int *b;
+		int generated[j];
+		p = generated;
+		b = generated;
+		generateArray(b, p, j, f * j);
+		clock_t beginb = clock();
+		medianBruteForce = bruteForceMedian(b, j);
+		clock_t endb = clock();
+		clock_t beginp = clock();
+		medianPartition = partitionMedian(p, j);
+		clock_t endp = clock();
+		double elapsedb = double(endb - beginb);
+		double elapsedp = double(endp - beginp);
+		totalp += comparisonsp;
+		totalb += comparisonsb;
+		totalTimep += elapsedp;
+		totalTimeb += elapsedb;
+		comparisonsb = 0;
+		comparisonsp = 0;
+	    }
+	    graphLog << j << "," << totalb/NUMTESTS << "," << totalTimeb/NUMTESTS << "," << medianBruteForce << ","  << totalp/NUMTESTS << "," << totalTimep/NUMTESTS << "," << medianPartition << endl;
+	    medianPartition = 0;
+	    medianBruteForce = 0;
+	    totalp = 0;
+	    totalb = 0;
+	    totalTimep = 0;
+	    totalTimeb = 0;
+	}
+	
+    }
+}
+
+void functionalTests()
+{
+    testLog.open("testLog.txt");
+
+    testLog << "Median Function Tests" << endl << endl;
+    
+    testLog << "Test 1 - Basic Array Even" << endl;
+    int basicArrayEven[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    testLog << "Testing: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(basicArrayEven, 10) << endl;
+    testLog << "PartitionMedian expects 6: " << partitionMedian(basicArrayEven, 10) << endl << endl;
+
+    testLog << "Test 2 - Basic Array Odd" << endl;
+    int basicArrayOdd[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    testLog << "Testing: 1, 2, 3, 4, 5, 6, 7, 8, 9" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(basicArrayOdd, 9) << endl;
+    testLog << "PartitionMedian expects 5: " << partitionMedian(basicArrayOdd, 9) << endl << endl;
+    
+    testLog << "Test 3 - Reverse Array Odd" << endl;
+    int reverseArrayOdd[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    testLog << "Testing: 9, 8, 7, 6, 5, 4, 3, 2, 1" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(reverseArrayOdd, 9) << endl;
+    testLog << "PartitionMedian expects 5: " << partitionMedian(reverseArrayOdd, 9) << endl << endl;
+
+    testLog << "Test 4 - Reverse Array Even" << endl;
+    int reverseArrayEven[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    testLog << "Testing: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(reverseArrayEven, 10) << endl;
+    testLog << "PartitionMedian expects 6: " << partitionMedian(reverseArrayEven, 10) << endl << endl;
+
+    testLog << "Test 5 - Duplicate Numbers Even" << endl;
+    int duplicateArrayEven[10] = {1, 2, 4, 4, 5, 6, 7, 8, 9, 10};
+    testLog << "Testing: 1, 2, 4, 4, 5, 6, 7, 8, 9, 10" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(duplicateArrayEven, 10) << endl;
+    testLog << "PartitionMedian expects 6: " << partitionMedian(duplicateArrayEven, 10) << endl << endl;
+
+    testLog << "Test 6 - Duplicate Numbers Odd" << endl;
+    int duplicateArrayOdd[9] = {1, 2, 4, 4, 5, 6, 7, 8, 9};
+    testLog << "Testing: 1, 2, 4, 4, 5, 6, 7, 8, 9" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(duplicateArrayOdd, 9) << endl;
+    testLog << "PartitionMedian expects 5: " << partitionMedian(duplicateArrayOdd, 9) << endl << endl;
+
+    testLog << "Test 7 - Random Values Odd" << endl;
+    int randomArray[9] = {2, 3, 6, 5, 8, 7, 1, 9, 4};
+    testLog << "Testing: 2, 3, 6, 5, 8, 7, 1, 9, 4" << endl;
+    testLog << "Brute force expects 5: " << bruteForceMedian(duplicateArrayOdd, 9) << endl;
+    testLog << "PartitionMedian expects 5: " << partitionMedian(duplicateArrayOdd, 9) << endl << endl;
+
+    testLog << "Array Generator Tests" << endl << endl;
+
+    testLog.close();
+}
+
+
